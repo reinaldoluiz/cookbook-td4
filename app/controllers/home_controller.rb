abd85242cfc2a0c61c288f95d11270ff1d9ee43c
reinaldo luiz <reinaldolluiz@gmail.com>
2020-08-01 00:05:22 -0300

    Aula 1h

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
-    def index
-    end
+  def index
+    @recipes = []
+    @recipes << Recipe.new(title: 'Bolo de banana', cuisine: 'Brasileira',
+                        recipe_type: 'Sobremesa', cook_time: 30)
+    @recipes << Recipe.new(title: 'Bolo de chocolate', cuisine: 'Brasileira',
+                        recipe_type: 'Sobremesa', cook_time: 30)
+    @recipes << Recipe.new(title: 'Bolo de cenoura', cuisine: 'Brasileira',
+                        recipe_type: 'Sobremesa', cook_time: 30)
+  end
 end
